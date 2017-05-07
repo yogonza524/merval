0b11507529a2cac2ee922ee6b685bb97ae93c37a
Gonzalo Mendoza <yogona524@gmail.com>
2017-05-07 00:28:45 -0300

    Fixed problems REST

--- src/main/java/com/core/merval/rest/ApplicationConfig.java
+++ src/main/java/com/core/merval/rest/ApplicationConfig.java
@@ -6,8 +6,18 @@
 
 package com.core.merval.rest;
 
+import java.io.IOException;
+import java.util.HashSet;
 import java.util.Set;
+import javax.ws.rs.ForbiddenException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+import org.primefaces.component.messages.Messages;
 
 /**
  * 
@@ -18,6 +28,19 @@ import javax.ws.rs.core.Application;
 @javax.ws.rs.ApplicationPath("rest")
 public class ApplicationConfig extends Application {
 
+    private Set<Object> singletons = new HashSet<Object>();
+    private HashSet<Class<?>> classes = new HashSet<Class<?>>();
+    
+    public ApplicationConfig() {
+        CorsFilter corsFilter = new CorsFilter();
+        corsFilter.getAllowedOrigins().add("*");
+        corsFilter.setAllowedMethods("OPTIONS, GET, POST, DELETE, PUT, PATCH");
+        singletons.add(corsFilter);
+
+    }
+    
+    
+
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new java.util.HashSet<>();
@@ -25,15 +48,147 @@ public class ApplicationConfig extends Application {
         return resources;
     }
 
-    /**
-     * Do not modify addRestResourceClasses() method.
-     * It is automatically populated with
-     * all resources defined in the project.
-     * If required, comment out calling this method in getClasses().
-     */
+
+    @PreMatching
+public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter
+{
+   protected boolean allowCredentials = true;
+   protected String allowedMethods;
+   protected String allowedHeaders;
+   protected String exposedHeaders;
+   protected int corsMaxAge = -1;
+   protected Set<String> allowedOrigins = new HashSet<String>();
+
+   /**
+    * Put "*" if you want to accept all origins
+    *
+    * @return
+    */
+   public Set<String> getAllowedOrigins()
+   {
+      return allowedOrigins;
+   }
+
+   /**
+    * Defaults to true
+    *
+    * @return
+    */
+   public boolean isAllowCredentials()
+   {
+      return allowCredentials;
+   }
+
+   public void setAllowCredentials(boolean allowCredentials)
+   {
+      this.allowCredentials = allowCredentials;
+   }
+
+   /**
+    * Will allow all by default
+    *
+    * @return
+    */
+   public String getAllowedMethods()
+   {
+      return allowedMethods;
+   }
+
+   /**
+    * Will allow all by default
+    * comma delimited string for Access-Control-Allow-Methods
+    *
+    * @param allowedMethods
+    */
+   public void setAllowedMethods(String allowedMethods)
+   {
+      this.allowedMethods = allowedMethods;
+   }
+
+   public String getAllowedHeaders()
+   {
+      return allowedHeaders;
+   }
+
+   /**
+    * Will allow all by default
+    * comma delimited string for Access-Control-Allow-Headers
+    *
+    * @param allowedHeaders
+    */
+   public void setAllowedHeaders(String allowedHeaders)
+   {
+      this.allowedHeaders = allowedHeaders;
+   }
+
+   public int getCorsMaxAge()
+   {
+      return corsMaxAge;
+   }
+
+   public void setCorsMaxAge(int corsMaxAge)
+   {
+      this.corsMaxAge = corsMaxAge;
+   }
+
+   public String getExposedHeaders()
+   {
+      return exposedHeaders;
+   }
+
+   /**
+    * comma delimited list
+    *
+    * @param exposedHeaders
+    */
+   public void setExposedHeaders(String exposedHeaders)
+   {
+      this.exposedHeaders = exposedHeaders;
+   }
+
+   @Override
+   public void filter(ContainerRequestContext requestContext) throws IOException
+   {
+      String origin = requestContext.getHeaderString(CorsHeaders.ORIGIN.name());
+      if (origin == null)
+      {
+         return;
+      }
+      if (!requestContext.getMethod().equalsIgnoreCase("OPTIONS"))
+      {
+         checkOrigin(requestContext, origin);
+      }
+   }
+
+   @Override
+   public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException
+   {
+      String origin = requestContext.getHeaderString(CorsHeaders.ORIGIN.name());
+      if (origin == null || requestContext.getMethod().equalsIgnoreCase("OPTIONS") || requestContext.getProperty("cors.failure") != null)
+      {
+         // don't do anything if origin is null, its an OPTIONS request, or cors.failure is set
+         return;
+      }
+      responseContext.getHeaders().putSingle(CorsHeaders.ACCESS_CONTROL_ALLOW_ORIGIN.name(), origin);
+      if (allowCredentials) responseContext.getHeaders().putSingle(CorsHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS.name(), "true");
+
+      if (exposedHeaders != null) {
+         responseContext.getHeaders().putSingle(CorsHeaders.ACCESS_CONTROL_EXPOSE_HEADERS.name(), exposedHeaders);
+      }
+   }
+
+
+   protected void checkOrigin(ContainerRequestContext requestContext, String origin)
+   {
+      if (!allowedOrigins.contains("*") && !allowedOrigins.contains(origin))
+      {
+         requestContext.setProperty("cors.failure", true);
+         throw new ForbiddenException(origin + " not allowed");
+      }
+   }
+}
+
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(com.core.merval.rest.Rest.class);
     }
-
-    
 }
---
+++ src/main/java/com/core/merval/rest/CorsHeaders.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.core.merval.rest;
+
+/**
+ *
+ * @author gonza
+ */
+public enum CorsHeaders {
+    ORIGIN, ACCESS_CONTROL_ALLOW_ORIGIN, ACCESS_CONTROL_EXPOSE_HEADERS,
+    ACCESS_CONTROL_REQUEST_METHOD,
+    ACCESS_CONTROL_ALLOW_METHODS,
+    ACCESS_CONTROL_REQUEST_HEADERS,
+    ACCESS_CONTROL_ALLOW_HEADERS,
+    ACCESS_CONTROL_MAX_AGE,
+    ACCESS_CONTROL_ALLOW_CREDENTIALS;
+    
+}
